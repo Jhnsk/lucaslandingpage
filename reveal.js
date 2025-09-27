e7a379449a00f38a965caf1b4b03d2f7bee5c2b5
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-09-26 22:57:51 -0300

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,46 @@
+window.revelar = ScrollReveal({ reset: true});
+
+revelar.reveal('.txt-h1-reveal',{
+    duration: 1000,
+    distance: '50px'
+});
+
+revelar.reveal('.txt-psico-reveal',{
+    duration: 1400,
+    distance: '50px'
+});
+
+revelar.reveal('.img-reveal',{
+    duration: 1600,
+    distance: '50px'
+});
+
+revelar.reveal('.name-reveal',{
+    duration: 1800,
+    distance: '50px'
+});
+
+revelar.reveal('.btn-reveal',{
+    duration: 2000,
+    distance: '50px'
+});
+
+revelar.reveal('.h2_reveal',{
+    duration: 2000,
+    distance: '30px'
+});
+
+revelar.reveal('.p_reveal',{
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.content_function',{
+    origin: screenLeft,
+    duration: 2000,
+    distance: '-90px'
+    
+});
+
+
+
